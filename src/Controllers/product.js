abcb6581089ce0580ee6eed1e5a10112b9b5e962
Duc-Tuan <phamductuan26032002@gmail.com>
2022-11-22 20:28:39 +0700

    update

--- src/Controllers/product.js
+++ src/Controllers/product.js
@@ -4,32 +4,32 @@ const fs = require("fs");
 const path = require("path");
 
 const getAll = (req, res, next) => {
-  var { page, pageSize } = req.query;
-  var skipNumber;
-  if (page || pageSize) {
-    pageSize = parseInt(pageSize);
-    page = parseInt(page);
-    if (page < 1) page = 1;
-    skipNumber = (page - 1) * pageSize;
-    Product.find()
-      .skip(skipNumber)
-      .limit(pageSize)
-      .then((data) => {
-        Product.countDocuments({})
-          .then((total) => {
-            var totalPage = Math.ceil(total / pageSize);
-            return res.status(200).json({ totalPage, data });
-          })
-          .catch((error) => next(error));
-      })
-      .catch((err) => next(err));
-  } else {
-    Product.find()
-      .then((data) => {
-        return res.status(200).json({ data });
-      })
-      .catch((err) => next(err));
-  }
+  // var { page, pageSize } = req.query;
+  // var skipNumber;
+  // if (page || pageSize) {
+  //   pageSize = parseInt(pageSize);
+  //   page = parseInt(page);
+  //   if (page < 1) page = 1;
+  //   skipNumber = (page - 1) * pageSize;
+  //   Product.find()
+  //     .skip(skipNumber)
+  //     .limit(pageSize)
+  //     .then((data) => {
+  //       Product.countDocuments({})
+  //         .then((total) => {
+  //           var totalPage = Math.ceil(total / pageSize);
+  //           return res.status(200).json({ totalPage, data });
+  //         })
+  //         .catch((error) => next(error));
+  //     })
+  //     .catch((err) => next(err));
+  // } else {
+  Product.find()
+    .then((data) => {
+      return res.status(200).json({ data });
+    })
+    .catch((err) => next(err));
+  // }
 };
 
 const addProduct = (req, res, next) => {
